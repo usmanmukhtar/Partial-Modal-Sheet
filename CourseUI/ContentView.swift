1e104c353a3a48345f3032e06c53024ca908bb04
Usman Mukhtar <usmanmukhtar432@gmail.com>
2020-07-12 13:44:21 +0500

    Initial Commit

---
+++ CourseUI/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  CourseUI
+//
+//  Created by Usman Mukhtar on 12/07/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CourseUI/CourseUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CourseUIApp.swift
+//  CourseUI
+//
+//  Created by Usman Mukhtar on 12/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct CourseUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
